6e72ddeb60e5d786a1fd55e38d9dc74772f03f1b
Chris Mems <christopher.mems@.com>
2023-03-09 13:06:46 -0600

    create drop-down for teams

--- src/Employees.jsx
+++ src/Employees.jsx
@@ -92,6 +92,16 @@ const Employees = () => {
   return (
     <main className="container">
       <div className='row justify-content-center mt-3 mb-3'>
+        <div className='col-6'>
+          <select className="form-select form-select-lg">
+            <option value="Team A">Team A</option>
+            <option value="Team B">Team B</option>
+            <option value="Team C">Team C</option>
+            <option value="Team D">Team D</option>
+          </select>
+        </div>
+      </div>
+      <div className='row justify-content-center mt-3 mb-3'>
         <div className='col-8'>
           <div className="card-collection">
             {
