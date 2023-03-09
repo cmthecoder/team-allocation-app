d77bfb4bc8c19aa44a6cf1b9432d9358c0700d87
Chris Mems <christopher.mems@.com>
2023-03-09 16:19:11 -0600

    create method to count the number of members in selected team

--- src/App.js
+++ src/App.js
@@ -108,7 +108,10 @@ function App() {
 
   return (
     <div>
-      <Header />
+      <Header
+        selectedTeam={selectedTeam}
+        teamMemberCount={employees.filter((employee) => employee.teamName === selectedTeam).length}
+      />
       <Employees
         employees={employees}
         selectedTeam={selectedTeam}
